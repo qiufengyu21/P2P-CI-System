c7b1f612d7f140894b36c38bd1696f253966505a
Qiufeng Yu <patrick_yudh@hotmail.com>
2018-04-10 11:10:47 -0400

    initial commit

---
+++ src/csc573/project1/CentralizedServer.java
@@ -0,0 +1,49 @@
+package csc573.project1;
+
+import java.io.*;
+import java.net.*;
+
+public class CentralizedServer extends Thread {
+	private static final int PORT_NUMBER = 7734;
+
+	private ServerSocket serverSocket;
+
+	public CentralizedServer(int port) throws IOException {
+		serverSocket = new ServerSocket(port);
+		serverSocket.setSoTimeout(10000);
+	}
+
+	public void run() {
+		while (true) {
+			try {
+				System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
+				Socket server = serverSocket.accept();
+
+				System.out.println("Just connected to " + server.getRemoteSocketAddress());
+				DataInputStream in = new DataInputStream(server.getInputStream());
+
+				System.out.println(in.readUTF());
+				DataOutputStream out = new DataOutputStream(server.getOutputStream());
+				out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress() + "\nGoodbye!");
+				server.close();
+
+			} catch (SocketTimeoutException s) {
+				System.out.println("Socket timed out!");
+				break;
+			} catch (IOException e) {
+				e.printStackTrace();
+				break;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int port = PORT_NUMBER;
+		try {
+			Thread t = new CentralizedServer(port);
+			t.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/csc573/project1/P2PClient.java
@@ -0,0 +1,31 @@
+package csc573.project1;
+
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class P2PClient {
+	public static final int PORT_NUMBER = 7734;
+
+	public static void main(String[] args) {
+		String serverName = "localhost";
+		int port = PORT_NUMBER;
+		try {
+			System.out.println("Connecting to " + serverName + " on port " + port);
+			Socket client = new Socket(serverName, port);
+
+			System.out.println("Just connected to " + client.getRemoteSocketAddress());
+			OutputStream outToServer = client.getOutputStream();
+			DataOutputStream out = new DataOutputStream(outToServer);
+
+			out.writeUTF("Hello from " + client.getLocalSocketAddress());
+			InputStream inFromServer = client.getInputStream();
+			DataInputStream in = new DataInputStream(inFromServer);
+
+			System.out.println("Server says " + in.readUTF());
+			client.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
